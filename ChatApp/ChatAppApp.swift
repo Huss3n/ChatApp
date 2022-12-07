c2b236b4321b5826816947dfd909a1fe6eb93bb3
Hussein <aisackhussein@gmail.com>
2022-12-07 12:33:16 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Muktar Hussein on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
